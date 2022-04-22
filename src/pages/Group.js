5bac51a36a9df3adeab80e6d0ff4d9193f308b04
venkatesh kumar <venkatesh.k@ampcome.com>
2022-04-22 18:17:24 +0530

    Added Group

--- src/pages/Group.js
+++ src/pages/Group.js
@@ -1,9 +1,47 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 import { useTitle } from "../components/customHooks/title";
 
 const Group = () => {
   useTitle("Groups");
-  return <div>Group</div>;
+  return (
+    <div className="mt-5 pb-5">
+      <Container>
+        <p
+          className="display-4 text-center text-primary mb-sm-5"
+          style={{ fontWeight: 400 }}
+        >
+          Group
+        </p>
+        <article className="mt-sm-5">
+          <h3>Ph.D. Positions :</h3>
+          <p className="text-justify">
+            We are looking for motivated Ph.D. students to join us. Candidates
+            with a valid JRF CSIR/UGC/DST-Inspire and interested in organic
+            synthesis, catalysis & medicinal chemistry can directly e-mail me (
+            <a href="mailto:kishore.natte@chy.iith.ac.in">
+              kishore.natte@chy.iith.ac.in
+            </a>
+            )
+          </p>
+        </article>
+
+        <article className="mt-5">
+          <h3>Postdoc Positions :</h3>
+          <p className="text-justify">
+            You can join the group with externally funded projects through SERB
+            National Postdoctoral Fellowship, UGC Postdoctoral Fellowship, and
+            DST Women Scientist. Interested candidates having Ph.D. degree in
+            organic synthesis or related areas can directly e-mail me (
+            <a href="mailto:kishore.natte@chy.iith.ac.in">
+              kishore.natte@chy.iith.ac.in
+            </a>
+            )
+          </p>
+        </article>
+      </Container>
+    </div>
+  );
 };
 
 export default Group;
